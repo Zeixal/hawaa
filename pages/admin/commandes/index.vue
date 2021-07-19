5d53317270374fe1268ef0c085b1ef608894caf0
bertyn <bertynboulikou@gmail.com>
2021-07-19 13:17:39 +0200

    ajout loading commande

--- pages/admin/commandes/index.vue
+++ pages/admin/commandes/index.vue
@@ -4,7 +4,7 @@
       <div class="px-4 md:px-10 mx-auto w-full"></div>
     </div>
     <section
-      class=" relative min-h-full  px-4 md:px-10 mx-auto w-full -m-24 z-30"
+      class=" relative min-h-full  px-4 md:px-10 mx-auto w-full -m-24 z-10"
     >
       <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -52,11 +52,66 @@
                     </th>
                   </tr>
                 </thead>
+                <tbody v-if="loading" class="bg-white divide-y divide-gray-200">
+                  <tr v-for="c in 6" :key="c">
+                    <td class="hidden sm:block px-6 py-4 whitespace-nowrap">
+                      <div
+                        class="h-4 w-6 bg-gray-200 animate-pulse rounded-sm  text-sm text-gray-900 truncate"
+                      ></div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0 h-10 w-10">
+                          <div
+                            class="h-10 w-10 bg-gray-200 animate-pulse rounded-full "
+                          ></div>
+                        </div>
+                        <div class="ml-4">
+                          <div
+                            class="h-4 w-5 bg-gray-200 animate-pulse rounded-t-sm text-sm font-medium text-gray-900"
+                          ></div>
+                          <div
+                            class="h-4 w-8 bg-gray-200 animate-pulse rounded-b-sm text-sm text-gray-500"
+                          ></div>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="hidden md:block px-6 py-4 whitespace-nowrap">
+                      <div
+                        class="h-4 w-6 bg-gray-200 animate-pulse rounded-sm  text-sm text-gray-900 md:w-28 truncate"
+                      ></div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <span
+                        class="h-4 w-6 bg-gray-200 animate-pulse px-2 inline-flex text-xs leading-5 font-semibold rounded-full  text-secondary-800"
+                      >
+                      </span>
+                    </td>
+                    <td
+                      class=" px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                    >
+                      <div
+                        class="h-4 w-6 bg-gray-200 animate-pulse rounded-sm"
+                      ></div>
+                    </td>
+                    <td
+                      class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
+                    >
+                      <a
+                        href="#"
+                        class="text-indigo-600 border border-blue-600 px-2 py-2 hover:bg-blue-700  rounded hover:text-gray-100"
+                        >Voir</a
+                      >
+                    </td>
+                  </tr>
+
+                  <!-- More people... -->
+                </tbody>
                 <tbody class="bg-white divide-y divide-gray-200">
-                  <tr>
+                  <tr v-for="o in orders" :key="o._id">
                     <td class="hidden sm:block px-6 py-4 whitespace-nowrap">
                       <div class="text-sm text-gray-900 truncate">
-                        01
+                        {{ o._id }}
                       </div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
@@ -70,29 +125,28 @@
                         </div>
                         <div class="ml-4">
                           <div class="text-sm font-medium text-gray-900">
-                            Jane
+                            {{ o.user.firstname || o.userRandom }}
                           </div>
-                          <div class="text-sm text-gray-500">Cooper</div>
+                          <div class="text-sm text-gray-500">
+                            {{ o.user.lastname }}
+                          </div>
                         </div>
                       </div>
                     </td>
                     <td class="hidden md:block px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm text-gray-900 md:w-28 truncate">
-                        Regional Paradigm Technician
-                        fuuifbfbdibiudbudbududbujidbdbd
-                      </div>
+                      <div class="text-sm text-gray-900 md:w-28 truncate"></div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
                       <span
                         class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-secondary-100 text-secondary-800"
                       >
-                        Remis
+                        {{ o.status }}
                       </span>
                     </td>
                     <td
                       class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
                     >
-                      10.90
+                      {{ o.total }}
                     </td>
                     <td
                       class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
@@ -112,17 +166,33 @@
           </div>
         </div>
       </div>
+      <footer-admin></footer-admin>
     </section>
-    <footer-admin></footer-admin>
   </main>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 import FooterAdmin from "~/components/admin/FooterAdmin.vue";
 export default {
   layout: "admin",
   components: {
     FooterAdmin
+  },
+  data() {
+    return {
+      loading: true,
+      orders: []
+    };
+  },
+  methods: {
+    ...mapActions({
+      getAllOrders: "ingredient/getAllOrders"
+    })
+  },
+  async fetch() {
+    /* this.orders = await this.getAllOrders();
+    this.loading = false; */
   }
 };
 </script>
